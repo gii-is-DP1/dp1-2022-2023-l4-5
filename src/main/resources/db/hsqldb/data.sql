de0535f0bb1815171786cdab50477b094263d439
PedroRuizAguilar <cocode2002@gmail.com>
2022-09-24 13:48:30 +0200

    Arregle el id

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -67,7 +67,8 @@ INSERT INTO owners VALUES (7, 'Jeff', 'Black', '1450 Oak Blvd.', 'Monona', '6085
 INSERT INTO owners VALUES (8, 'Maria', 'Escobito', '345 Maple St.', 'Madison', '6085557683', 'owner1');
 INSERT INTO owners VALUES (9, 'David', 'Schroeder', '2749 Blackhawk Trail', 'Madison', '6085559435', 'owner1');
 INSERT INTO owners VALUES (10, 'Carlos', 'Estaban', '2335 Independence La.', 'Waunakee', '6085555487', 'owner1');
-INSERT INTO owners VALUES (11, 'Pedro', 'Ruiz', '123 Fake street', 'Yokai Watch', '6085555487', 'pedruiagu1');
+-- pedruiagu1 owner
+INSERT INTO owners VALUES (14, 'Pedro', 'Ruiz', '123 Fake street', 'Yokai Watch', '6085555487', 'pedruiagu1');
 
 -- Yuuta094 pets
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (14, 'Milo', '2020-10-20', 2, 11);
@@ -89,7 +90,8 @@ INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (10, 'Mulligan', '2
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (11, 'Freddy', '2010-03-09', 5, 9);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (12, 'Lucky', '2010-06-24', 2, 10);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (13, 'Sly', '2012-06-08', 1, 10);
-INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (13, 'Mordisquitos', '2012-06-08', 1, 11);
+-- pedruiagu1 pets
+INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (13, 'Mordisquitos', '2012-06-08', 1, 14);
 
 -- Visitas
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (1, 7, '2013-01-01', 'rabies shot');
