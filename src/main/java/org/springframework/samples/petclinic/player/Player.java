f7a7dfaeed67dd4390b2ebb3c69e7d7e938810b1
Yuuta094 <YvanSan094@gmail.com>
2022-10-25 21:47:12 +0200

    Relaciones de Player actualizado

--- src/main/java/org/springframework/samples/petclinic/player/Player.java
+++ src/main/java/org/springframework/samples/petclinic/player/Player.java
@@ -2,10 +2,13 @@ package org.springframework.samples.petclinic.player;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.samples.petclinic.card.ability.Ability;
 import org.springframework.samples.petclinic.card.hero.Heroe;
 import org.springframework.samples.petclinic.model.NamedEntity;
 
 import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -20,8 +23,12 @@ public class Player extends NamedEntity {
     private Boolean evasion;
     private Integer numOrcsKilled;
     private Integer numWarLordKilled;
+    private Integer  order;
 
     //Relaciones
     @OneToMany(cascade = CascadeType.ALL)
-    private Set<Heroe> heroe;
+    private Set<Heroe> heroes;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Ability> abilities;
 }
