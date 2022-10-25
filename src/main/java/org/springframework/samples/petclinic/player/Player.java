0cff8fab53ae0e1b9a74992e724dac8e56941af9
Yuuta094 <YvanSan094@gmail.com>
2022-10-25 21:16:50 +0200

    Getters/Setter/Relaciones de Player

--- src/main/java/org/springframework/samples/petclinic/player/Player.java
+++ src/main/java/org/springframework/samples/petclinic/player/Player.java
@@ -2,19 +2,40 @@ package org.springframework.samples.petclinic.player;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.samples.petclinic.card.hero.Heroe;
 import org.springframework.samples.petclinic.model.NamedEntity;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Getter
 @Setter
+@Table(name= "Player")
 public class Player extends NamedEntity {
 
+    //Propiedades
     private Integer gold;
     private Integer glory;
     private Boolean evasion;
     private Integer numOrcsKilled;
     private Integer numWarLordKilled;
 
+    //Getters
+    public Integer getGold() { return gold; }
+    public Integer getGlory() { return glory; }
+    public Boolean getEvasion() { return evasion; }
+    public Integer getNumOrcsKilled() { return numOrcsKilled; }
+    public Integer getNumWarLordKilled() { return numWarLordKilled; }
+
+    //Setters
+    public void setGold(Integer gold) { this.gold = gold; }
+    public void setGlory(Integer glory) { this.glory = glory; }
+    public void setEvasion(Boolean evasion) { this.evasion = evasion; }
+    public void setNumOrcsKilled(Integer numOrcsKilled) { this.numOrcsKilled = numOrcsKilled; }
+    public void setNumWarLordKilled(Integer numWarLordKilled) {this.numWarLordKilled = numWarLordKilled; }
+
+    //Relaciones
+    @OneToMany(cascade = CascadeType.ALL)
+    private Set<Heroe> heroe;
 }
