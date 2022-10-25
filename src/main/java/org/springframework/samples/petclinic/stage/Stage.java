9ddc8593c9abd5f2cdbb5ea4349c23edcd41362e
IsmaelRuizJurado <ismruijur@alum.us.es>
2022-10-25 22:50:50 +0200

    Relaciones Stage y atributos GameManager

---
+++ src/main/java/org/springframework/samples/petclinic/stage/Stage.java
@@ -0,0 +1,21 @@
+package org.springframework.samples.petclinic.stage;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.samples.petclinic.card.Card;
+import org.springframework.samples.petclinic.gameManager.GameManager;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+@Getter
+@Setter
+@Entity
+public class Stage extends Card {
+
+    @ManyToOne(optional=true)
+    private GameManager gameManager;
+
+    @ManyToOne(optional=false)
+    private GameManager gameManager2;
+}
