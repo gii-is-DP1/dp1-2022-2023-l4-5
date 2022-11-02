f19fd4be6f96ea4972024da2c958df566a0dc5a2
Alejandro Santiago Félix <alex.0002002@gmail.com>
2022-11-02 15:39:54 +0100

    Faltaba el deny all.

--- src/main/java/org/springframework/samples/petclinic/configuration/SecurityConfiguration.java
+++ src/main/java/org/springframework/samples/petclinic/configuration/SecurityConfiguration.java
@@ -42,13 +42,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/admin/**").hasAnyAuthority("DOKTOL")
             .antMatchers("/owners/**").hasAnyAuthority("DOKTOL")
             .antMatchers("/vets/**").authenticated()
+            .anyRequest().denyAll()
             .and()
             .formLogin()
             /*.loginPage("/login")*/
             .failureUrl("/login-error")
             .and()
-					.logout()
-						.logoutSuccessUrl("/");
+            .logout()
+            .logoutSuccessUrl("/");
                 // Configuración para que funcione la consola de administración
                 // de la BD H2 (deshabilitar las cabeceras de protección contra
                 // ataques de tipo csrf y habilitar los framesets si su contenido
