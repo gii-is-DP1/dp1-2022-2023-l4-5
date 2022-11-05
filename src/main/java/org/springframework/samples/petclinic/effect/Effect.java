fb5c3e2a310ba7ce3e61608e831bf3658630fb41
PedroRuizAguilar <cocode2002@gmail.com>
2022-11-05 16:38:29 +0100

    Effect bla bla

--- src/main/java/org/springframework/samples/petclinic/effect/Effect.java
+++ src/main/java/org/springframework/samples/petclinic/effect/Effect.java
@@ -2,12 +2,46 @@ package org.springframework.samples.petclinic.effect;
 
 
 
+import org.springframework.samples.petclinic.card.enemy.Enemy;
 import org.springframework.samples.petclinic.enumer.Phase;
+import org.springframework.samples.petclinic.player.Player;
+
+import java.util.Random;
 
 
 public abstract class Effect {
 
     private Phase phase;
+    private Player player;
+    public void robarCartas(Integer n){
+        for(int i=0; i<n; i++){
+            this.player.hand.add(this.player.deck.get(0));
+            this.player.deck.remove(0);
+        }
+    }
+    public void damageToEnemy(Integer damage, Enemy enemy){
+        enemy.setHealth(enemy.getHealth-damage);
+    }
+    public void loseCards(Integer n){
+        for(int i=0; i<n; i++){
+            this.player.hand.remove(0);
+        }
+    }
+    public void damageToHero(Enemy enemy){
+        n=enemy.getHealth();
+        for(int i=0; i<n; i++){
+            this.player.deck.remove(0);
+            if(player.deck.isEmpty()){
+                player.getHero().setHealth(player.getHero().health-1);
+                if(player.getHero().health==0){
+                    player.setHero(null);
+                }
+            }
+        }
+    }
+    public void preventDamageEnemy(Enemy enemy){
+
+    }
 
     abstract void useEffect();
 }
