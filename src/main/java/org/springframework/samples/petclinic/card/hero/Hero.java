2fec9ca0b0ab33fff9160650d8eaa4a11989fd3e
Laura <xxlaurarmxx@gmail.com>
2022-11-08 20:20:31 +0100

    Implementación de 2 cosos nuevos del database

--- src/main/java/org/springframework/samples/petclinic/card/hero/Hero.java
+++ src/main/java/org/springframework/samples/petclinic/card/hero/Hero.java
@@ -23,9 +23,9 @@ public class Hero extends Card {
     @Enumerated(EnumType.STRING)
     private Role role;
 
-    @OneToMany
+    @ManyToMany
     private List<Ability> abilities;
 
-    @OneToMany(cascade = CascadeType.ALL)
+    @ManyToMany(cascade = CascadeType.ALL)
     private List<Capacity> capacities;
 }
