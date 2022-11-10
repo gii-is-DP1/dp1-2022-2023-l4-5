ac81b583ef6e68c92e2c8d1f16c57b2476e00516
Alejandro Santiago Félix <alex.0002002@gmail.com>
2022-11-10 15:30:30 +0100

    Arreglado error que impedía ver el listado.

--- src/main/java/org/springframework/samples/petclinic/user/User.java
+++ src/main/java/org/springframework/samples/petclinic/user/User.java
@@ -1,20 +1,19 @@
 package org.springframework.samples.petclinic.user;
 
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.Value;
-import net.bytebuddy.implementation.bind.annotation.Default;
 import org.hibernate.validator.constraints.URL;
-import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.player.Tier;
 import org.springframework.samples.petclinic.statistic.Statistic;
 
 import javax.persistence.*;
-import javax.validation.constraints.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.Date;
 import java.util.Set;
 
@@ -40,14 +39,14 @@ public class User extends BaseEntity {
     @URL
     private String avatar;
 
-    @Enumerated(EnumType.STRING)
-    private Tier tier; // TODO: Poner un valor por defecto.
+    @Enumerated
+    private Tier tier;
 
     // TODO: Decidir tamaño.
     @NotBlank
     private String description;
 
-    @Enumerated(EnumType.STRING)
+    @Enumerated
     @NotNull
     private Authority authority;
 
