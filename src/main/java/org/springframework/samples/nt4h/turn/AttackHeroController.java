147984a7f896d44af46aae04f4ea09a8a73950e8
Yuuta094 <YvanSan094@gmail.com>
2022-11-14 21:03:20 +0100

    AttackHeroController.java y cardAboutEnemy.jsp

--- src/main/java/org/springframework/samples/nt4h/turn/AttackHeroController.java
+++ src/main/java/org/springframework/samples/nt4h/turn/AttackHeroController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.lang.reflect.Array;
 
 @Controller
