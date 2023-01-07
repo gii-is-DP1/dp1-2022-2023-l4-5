a4b3a0da00066440dafd30c10a4bc04e53769920
Yuuta094 <YvanSan094@gmail.com>
2023-01-07 21:01:12 +0100

    AuditoryGame

--- src/main/java/org/springframework/samples/nt4h/game/AuditoryGame.java
+++ src/main/java/org/springframework/samples/nt4h/game/AuditoryGame.java
@@ -3,14 +3,29 @@ package org.springframework.samples.nt4h.game;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.envers.Audited;
+import org.hibernate.envers.NotAudited;
 import org.springframework.samples.nt4h.model.BaseEntity;
+import org.springframework.samples.nt4h.player.Player;
+import org.springframework.samples.nt4h.user.User;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
-@Setter
-@Getter
-@Entity
 @Audited
-
+@Entity
+@Getter
+@Setter
 public class AuditoryGame extends BaseEntity {
+
+    @OneToOne
+        @NotAudited
+    Game game;
+    @OneToOne
+        @NotAudited
+    User user;
+
+
+
+
 }
