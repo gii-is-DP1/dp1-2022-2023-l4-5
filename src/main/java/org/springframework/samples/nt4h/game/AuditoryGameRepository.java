ab577345aec5d27e57c80fc2d0c0b04430d9ec2f
Yuuta094 <YvanSan094@gmail.com>
2023-01-07 21:01:35 +0100

    AuditoryGameRepository

---
+++ src/main/java/org/springframework/samples/nt4h/game/AuditoryGameRepository.java
@@ -0,0 +1,7 @@
+package org.springframework.samples.nt4h.game;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuditoryGameRepository extends CrudRepository<AuditoryGame, Integer> {
+
+}
