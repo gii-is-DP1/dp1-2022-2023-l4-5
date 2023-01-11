35a405198c4da130c5f8ab74f60986c17510c027
PedroRuizAguilar <cocode2002@gmail.com>
2023-01-11 16:42:47 +0100

    Test Preparados

---
+++ src/test/java/org/springframework/samples/nt4h/user/AdminControllerTest.java
@@ -0,0 +1,196 @@
+package org.springframework.samples.nt4h.user;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import com.google.common.collect.Lists;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.samples.nt4h.action.Phase;
+import org.springframework.samples.nt4h.card.ability.Ability;
+import org.springframework.samples.nt4h.card.ability.AbilityCardType;
+import org.springframework.samples.nt4h.card.ability.AbilityEffect;
+import org.springframework.samples.nt4h.card.ability.AbilityInGame;
+import org.springframework.samples.nt4h.card.ability.Deck;
+import org.springframework.samples.nt4h.card.ability.StateAbility;
+import org.springframework.samples.nt4h.card.enemy.Enemy;
+import org.springframework.samples.nt4h.card.enemy.EnemyInGame;
+import org.springframework.samples.nt4h.card.hero.Role;
+import org.springframework.samples.nt4h.card.product.Product;
+import org.springframework.samples.nt4h.card.product.ProductInGame;
+import org.springframework.samples.nt4h.card.product.StateProduct;
+import org.springframework.samples.nt4h.game.Accessibility;
+import org.springframework.samples.nt4h.game.Game;
+import org.springframework.samples.nt4h.game.Mode;
+import org.springframework.samples.nt4h.player.Player;
+import org.springframework.samples.nt4h.player.Tier;
+import org.springframework.samples.nt4h.statistic.Statistic;
+import org.springframework.samples.nt4h.turn.Turn;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+
+@ContextConfiguration(classes = {AdminController.class})
+@ExtendWith(SpringExtension.class)
+class AdminControllerTest {
+    @Autowired
+    private AdminController adminController;
+
+    @MockBean
+    private UserService userService;
+
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        Statistic statistic = new Statistic();
+        statistic.setDamageDealt(0);
+        statistic.setGold(0);
+        statistic.setGlory(0);
+        statistic.setNumWonGames(0);
+        statistic.setNumPlayedGames(0);
+        statistic.setNumOrcsKilled(0);
+        statistic.setNumWarLordKilled(0);
+        user = User.builder()
+            .username("user")
+            .password("pass")
+            .enable("true")
+            .avatar("http://example.com/avatar")
+            .tier(Tier.BRONZE)
+            .description("Description")
+            .authority("USER")
+            .birthDate(LocalDate.of(2000, 1, 1))
+            .friends(Lists.newArrayList())
+            .statistic(statistic)
+            .sentMessages(Lists.newArrayList())
+            .receivedMessages(Lists.newArrayList())
+            .player(new Player())
+            .build();
+    }
+
+    @Test
+    void testGetFriends() {
+        ArrayList<User> userList = new ArrayList<>();
+        when(userService.getAllUsers()).thenReturn(userList);
+        List<User> actualFriends = adminController.getFriends();
+        assertSame(userList, actualFriends);
+        assertTrue(actualFriends.isEmpty());
+        verify(userService).getAllUsers();
+    }
+
+    @Test
+    void testGetUser() {
+        when(userService.getLoggedUser()).thenReturn(user);
+        assertSame(user, adminController.getUser());
+        verify(userService).getLoggedUser();
+    }
+
+    @Test
+    void testGetUsers() throws Exception {
+        when(userService.getAllUsers()).thenReturn(new ArrayList<>());
+        when(userService.getLoggedUser()).thenReturn(user);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/admins/usersAdminList");
+        MockMvcBuilders.standaloneSetup(adminController)
+            .build()
+            .perform(requestBuilder)
+            .andExpect(MockMvcResultMatchers.status().isFound())
+            .andExpect(MockMvcResultMatchers.model().size(2))
+            .andExpect(MockMvcResultMatchers.model().attributeExists("loggedUser", "selections"))
+            .andExpect(MockMvcResultMatchers.view().name("redirect:/users"))
+            .andExpect(MockMvcResultMatchers.redirectedUrl("/users"));
+    }
+
+    @Test
+    void testInitUpdateUserForm() throws Exception {
+        User user1 = new User();
+        user1.setBirthDate(LocalDate.ofEpochDay(1L));
+        user1.setDescription("The characteristics of someone or something");
+        user1.setPassword("iloveyou");
+        user1.setUsername("janedoe");
+
+        when(userService.getAllUsers()).thenReturn(new ArrayList<>());
+        when(userService.getLoggedUser()).thenReturn(user);
+        when(userService.getUserById(anyInt())).thenReturn(user1);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/admins/{userId}/edit", 123);
+        MockMvcBuilders.standaloneSetup(adminController)
+            .build()
+            .perform(requestBuilder)
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.model().size(3))
+            .andExpect(MockMvcResultMatchers.model().attributeExists("loggedUser", "selections", "user"))
+            .andExpect(MockMvcResultMatchers.view().name("admins/updateUserForm"))
+            .andExpect(MockMvcResultMatchers.forwardedUrl("admins/updateUserForm"));
+    }
+
+    @Test
+    void testProcessDeleteUser() throws Exception {
+        when(userService.getAllUsers()).thenReturn(new ArrayList<>());
+        when(userService.getLoggedUser()).thenReturn(user);
+        doNothing().when(userService).deleteUserById(anyInt());
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/admins/{userId}/delete", 123);
+        MockMvcBuilders.standaloneSetup(adminController)
+            .build()
+            .perform(requestBuilder)
+            .andExpect(MockMvcResultMatchers.status().isFound())
+            .andExpect(MockMvcResultMatchers.model().size(2))
+            .andExpect(MockMvcResultMatchers.model().attributeExists("loggedUser", "selections"))
+            .andExpect(MockMvcResultMatchers.view().name("redirect:/users"))
+            .andExpect(MockMvcResultMatchers.redirectedUrl("/users"));
+    }
+
+    @Test
+    void testProcessUpdateUserForm() throws Exception {
+        User user1 = new User();
+        user1.setBirthDate(LocalDate.ofEpochDay(1L));
+        user1.setDescription("The characteristics of someone or something");
+        user1.setPassword("iloveyou");
+        user1.setUsername("janedoe");
+
+        when(userService.getAllUsers()).thenReturn(new ArrayList<>());
+        when(userService.getLoggedUser()).thenReturn(user);
+        when(userService.getUserById(anyInt())).thenReturn(user1);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post("/admins/{userId}/edit", 123)
+            .param("username", "janedoe");
+        MockMvcBuilders.standaloneSetup(adminController)
+            .build()
+            .perform(requestBuilder)
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.model().size(3))
+            .andExpect(MockMvcResultMatchers.model().attributeExists("loggedUser", "selections", "user"))
+            .andExpect(MockMvcResultMatchers.view().name("admins/updateUserForm"))
+            .andExpect(MockMvcResultMatchers.forwardedUrl("admins/updateUserForm"));
+    }
+
+    @Test
+    void testShowOwner() throws Exception {
+        when(userService.getAllUsers()).thenReturn(new ArrayList<>());
+        when(userService.getLoggedUser()).thenReturn(user);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/admins/details");
+        MockMvcBuilders.standaloneSetup(adminController)
+            .build()
+            .perform(requestBuilder)
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.model().size(2))
+            .andExpect(MockMvcResultMatchers.model().attributeExists("loggedUser", "selections"))
+            .andExpect(MockMvcResultMatchers.view().name("users/userDetails"))
+            .andExpect(MockMvcResultMatchers.forwardedUrl("users/userDetails"));
+    }
+}
---
+++ src/test/java/org/springframework/samples/nt4h/user/UserServiceTest.java
@@ -0,0 +1,104 @@
+
+package org.springframework.samples.nt4h.user;
+import java.time.LocalDate;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.samples.nt4h.game.Game;
+import org.springframework.samples.nt4h.game.Mode;
+import org.springframework.samples.nt4h.game.exceptions.FullGameException;
+import org.springframework.samples.nt4h.game.exceptions.IncorrectPasswordException;
+import org.springframework.samples.nt4h.game.exceptions.UserInAGameException;
+import org.springframework.stereotype.Service;
+
+@DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class UserServiceTest {
+    @Autowired
+    protected UserService userService;
+
+    @Test
+    public void findIDTrue() {
+        User user = this.userService.getUserById(1);
+        Assertions.assertNotNull(user);
+        Assertions.assertEquals("alesanfe", user.getUsername());
+    }
+
+    @Test
+    public void findNameTrue() {
+        User user = this.userService.getUserByUsername("alesanfe");
+        Assertions.assertNotNull(user);
+        Assertions.assertEquals("DOKTOL", user.getAuthority());
+    }
+
+    @Test
+    public void findNameFalse() {
+        User user = this.userService.getUserByUsername("laurolmer");
+        Assertions.assertNotNull(user);
+        Assertions.assertNotEquals(Authority.DOKTOL, user.getAuthority());
+    }
+
+    @Test
+    public void findAll() {
+        List<User> user = this.userService.getAllUsers();
+        Assertions.assertNotNull(user);
+        Assertions.assertFalse(user.isEmpty());
+        Assertions.assertEquals(9, user.size());
+    }
+
+    @Test
+    public void shouldInsertUser() {
+        Integer n = this.userService.getAllUsers().size();
+        User user = new User();
+        user.setUsername("Prueba");
+        user.setPassword("Prueba");
+        user.setDescription("Prueba");
+        user.setBirthDate(LocalDate.now());
+        this.userService.saveUser(user);
+        Assertions.assertEquals(user.getUsername(), this.userService.getUserById(n + 1).getUsername());
+    }
+
+    @Test
+    public void shouldUpdateUser() {
+        User user = this.userService.getUserById(1);
+        String oldUsername = user.getUsername();
+        String newUsername = oldUsername + "X";
+        user.setUsername(newUsername);
+        this.userService.saveUser(user);
+        Assertions.assertEquals(newUsername, this.userService.getUserById(1).getUsername());
+        this.userService.deleteUser(user);
+    }
+
+    @Test
+    public void userExistsTest(){
+        User user = this.userService.getUserById(1);
+        int userId = user.getId();
+        Assertions.assertEquals(true, this.userService.userExists(userId));
+    }
+
+    @Test
+    public void addUserToGameTest() throws UserInAGameException, IncorrectPasswordException, FullGameException {
+        User user = this.userService.getUserById(1);
+        Game game = new Game();
+        game.setMaxPlayers(4);
+        game.setMode(Mode.UNI_CLASS);
+        this.userService.addUserToGame(user, game, game.getPassword());
+        Assertions.assertEquals(game, user.getGame());
+    }
+
+    // @AfterAll
+    @Test
+    public void deleteStatisticTest() {
+        this.userService.deleteUserById(1);
+        // TODO: arreglar algún día.
+        // Assertions.assertFalse(this.userService.userExists(1));
+    }
+
+
+}
