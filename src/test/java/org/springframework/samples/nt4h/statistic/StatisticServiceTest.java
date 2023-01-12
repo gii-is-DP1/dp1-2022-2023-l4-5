fd6ecafaad925fac33341ad8c57dfd0794209c10
IsmaelRuizJurado <ismruijur@alum.us.es>
2023-01-12 20:18:30 +0100

    Actualización de tests

--- src/test/java/org/springframework/samples/nt4h/statistic/StatisticServiceTest.java
+++ src/test/java/org/springframework/samples/nt4h/statistic/StatisticServiceTest.java
@@ -4,8 +4,10 @@ package org.springframework.samples.nt4h.statistic;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.samples.nt4h.exceptions.NotFoundException;
+import org.springframework.samples.nt4h.message.Advise;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,6 +20,10 @@ public class StatisticServiceTest {
     @Autowired
     StatisticService statisticService;
     private Integer idStatistic;
+
+    @MockBean
+    private Advise advise;
+
     @BeforeEach
     void setUp() {
         Statistic statistic= Statistic.createStatistic();
