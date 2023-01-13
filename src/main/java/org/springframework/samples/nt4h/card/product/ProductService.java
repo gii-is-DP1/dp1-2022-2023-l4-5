870668d10430d15c25fc18653c6ce47b4db06755
Alejandro Santiago Félix <alex.0002002@gmail.com>
2023-01-13 19:59:19 +0100

    :bug: Arreglado bug en los señores de la guerra.

--- src/main/java/org/springframework/samples/nt4h/card/product/ProductService.java
+++ src/main/java/org/springframework/samples/nt4h/card/product/ProductService.java
@@ -42,8 +42,8 @@ public class ProductService {
             .flatMap(heroInGame -> heroInGame.getHero().getCapacities().stream().map(Capacity::getStateCapacity))
             .collect(Collectors.toList());
         List<StateCapacity> capacitiesNeeded = productInGame.getProduct().getCapacity().stream().map(Capacity::getStateCapacity).collect(Collectors.toList());
-        if (!new HashSet<>(stateCapacities).containsAll(capacitiesNeeded))
-            throw new CapacitiesRequiredException();
+
+
         if (Objects.requireNonNull(selectedProduct.getStateProduct()) == StateProduct.IN_SALE) {
             if (player.getStatistic().getGold() < selectedProduct.getProduct().getPrice())
                 throw new NoMoneyException();
